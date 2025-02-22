22419ceae0a45de089ee2b38d265d7416d237dc0
Carson <carson.m.a.r.souza@gmail.com>
2025-02-22 11:44:52 -0300

    reorganização dos arquivos do projeto

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -1,5 +1,8 @@
 import unittest
 
+def adicionar_item(nome_item, quantidade):
+    return f"{quantidade}x {nome_item} adicionado ao carrinho."
+
 class TestMenu(unittest.TestCase):
     def test_item_adicionado(self):
         self.assertEqual(adicionar_item("Café Expresso", 2), "2x Café Expresso adicionado ao carrinho.")
